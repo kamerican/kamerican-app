95532f79a22054cecbfb2175b4f39ce9c50a9564
kamerican <44129995+kamerican@users.noreply.github.com>
2018-10-19 00:08:22 -0500

    add basic html

--- kamericanapp/routes.py
+++ kamericanapp/routes.py
@@ -1,9 +1,23 @@
+from flask import render_template
 from kamericanapp import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'kamerican'}
+    posts = [
+        {
+            'author': {'username': 'test1'},
+            'body': '123'
+        },
+        {
+            'author': {'username': 'test2'},
+            'body': '456'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
