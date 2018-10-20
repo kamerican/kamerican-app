dd280558c4e5b57a8559d47601f169ec6c726151
kamerican <44129995+kamerican@users.noreply.github.com>
2018-10-20 00:46:09 -0500

    add login forms

--- kamericanapp/routes.py
+++ kamericanapp/routes.py
@@ -1,9 +1,13 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from kamericanapp import app
+from kamericanapp.forms import LoginForm
 
 @app.route('/')
+def RedirectToIndex():
+    return redirect('/index')
+
 @app.route('/index')
-def index():
+def Index():
     user = {'username': 'kamerican'}
     posts = [
         {
@@ -17,10 +21,23 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            'Login requested for user {}, remember_me={}'.format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
+
+
+
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
