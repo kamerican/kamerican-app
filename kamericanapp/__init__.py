221a54451a561baac27283f5f8c64e66618a963b
kamerican <44129995+kamerican@users.noreply.github.com>
2018-10-20 12:45:32 -0500

    database boilerplate pre init

--- kamericanapp/__init__.py
+++ kamericanapp/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-
+# Initialize web server gateway interface (WSGI) web framework
 app = Flask(__name__)
+
+# Load flask configuration
 app.config.from_object(Config)
 
+# Initialize database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+
+# Load python application modules
 from kamericanapp import routes
 
 
