be5a661c45cbfc2a61242eb85a752061a28f1cff
kamerican <44129995+kamerican@users.noreply.github.com>
2018-10-20 16:32:49 -0500

    db post table migrate

--- kamericanapp/models.py
+++ kamericanapp/models.py
@@ -1,4 +1,5 @@
 from kamericanapp import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,5 +7,18 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    # "one" side in one-to-many relationship
+    # makes an author field for each Post
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
