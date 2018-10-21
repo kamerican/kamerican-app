f0d5ab82b327e836ffb2aaf7209c52332518f748
kamerican <44129995+kamerican@users.noreply.github.com>
2018-10-21 00:10:50 -0500

    add avatars to profile page

--- kamericanapp/models.py
+++ kamericanapp/models.py
@@ -2,6 +2,8 @@ from kamericanapp import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
+
 
 @login.user_loader
 def load_user(id):
@@ -17,6 +19,9 @@ class User(UserMixin, db.Model):
     # makes an author field for each Post
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
     def check_password(self, password):
--- kamericanapp/routes.py
+++ kamericanapp/routes.py
@@ -65,5 +65,15 @@ def register():
         else:
             return render_template('register.html', title='Register', form=form)
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
 if __name__ == "__main__":
     app.run(debug=True)
